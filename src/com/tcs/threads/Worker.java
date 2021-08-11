eac229acb5093cb6d6730d508ad001988d4f8dd4
tcsswapnika <tcsswapnika@gmail.com>
2021-08-11 10:52:46 +0000

    multi threading

--- src/com/tcs/threads/Worker.java
+++ src/com/tcs/threads/Worker.java
@@ -5,6 +5,12 @@ public class Worker implements Runnable {
 	@Override
 	public void run() {
 		System.out.println(Thread.currentThread());
+		try {
+			Thread.sleep(9999);
+		} catch (InterruptedException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 	}
 
 }
